81e1ae1e840d9a72fb42f0d0313a87b54824e575
olomi.store <olomi.store@gmail.com>
2025-10-08 19:29:53 +0000

    Ajuste do firebase

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,18 +1,25 @@
-// Importa as funções necessárias dos SDKs do Firebase
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-firestore.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-storage.js";
-import { getFunctions } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-functions.js";
 
-const response = await fetch('/__/firebase/init.json');
-const firebaseConfig = await response.json();
+const firebaseConfig = {
+  apiKey: "AIzaSyCiiWyMqDmF17aQBA-fwNo5ByotEsA7fn0",
+  authDomain: "olomi-7816a.firebaseapp.com",
+  databaseURL: "https://olomi-7816a-default-rtdb.firebaseio.com",
+  projectId: "olomi-7816a",
+  storageBucket: "olomi-7816a.firebasestorage.app",
+  messagingSenderId: "562685499782",
+  appId: "1:562685499782:web:23616d2db4738093c43407",
+  measurementId: "G-2FSC9P97MX"
+};
 
-// Inicializa o Firebase
+// Não altere mais nada abaixo desta linha
+// =================================================================================
+
 const app = initializeApp(firebaseConfig);
 
-// Exporta os serviços do Firebase individualmente para que outros módulos possam usá-los
+// Todos estes serviços irão agora apontar para o projeto correto.
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
-export const functions = getFunctions(app);
